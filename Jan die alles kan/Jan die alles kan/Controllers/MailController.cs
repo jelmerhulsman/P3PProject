24a5570da5228e3d2e78006bbefea9088c6fde7b
barryMuntjes <sch.wdb@gmail.com>
2014-03-06 15:50:02 +0100

    Mailcontroller static interne functie

--- Jan die alles kan/Jan die alles kan/Controllers/MailController.cs
+++ Jan die alles kan/Jan die alles kan/Controllers/MailController.cs
@@ -5,14 +5,12 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net;
 using System.Net.Mail;
+using Jan_die_alles_kan.Models;
 
 namespace Jan_die_alles_kan.Controllers
 {
     public class MailController : Controller
     {
-       
-           
-        
         [HttpPost]
         public ActionResult SendMail()
         {
@@ -33,6 +31,32 @@ namespace Jan_die_alles_kan.Controllers
             Mail.Body = "this is my test email body";
             client.Send(Mail);
              return Redirect("/page/index");
+        }
+
+        public static void SendMailInner(string username)
+        {
+            UserDataContext db = new UserDataContext();
+
+            var EmailAddress = from user in db.DBUserData
+                       where user.Username == username
+                       select user.Email;
+
+            string email = EmailAddress.ToList().First();
+            string subject = "test";
+            string emailFrom = "developdejong@gmail.com";
+            string password = "darktranquillity";
+            MailMessage Mail = new MailMessage(emailFrom, email);
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.gmail.com";
+            client.Credentials = new NetworkCredential(emailFrom, password);
+            client.EnableSsl = true;
+            Mail.Subject = subject;
+            Mail.To.Add(email);
+            Mail.Body = "this is my test email body";
+            client.Send(Mail);
         }
     }
 }
