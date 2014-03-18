f40a76aa69587b22713ed554a10889afe6fd85ec
Jelmer <jelmer_hulsman@hotmail.com>
2014-03-18 14:14:02 +0100

    Gewoon doen zegt Dylan

---
+++ Jan die alles kan/Jan die alles kan/Controllers/AjaxController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jan_die_alles_kan.Controllers
+{
+    public class AjaxController : Controller
+    {
+        //
+        // GET: /Ajax/
+
+        [HttpPost]
+        public ActionResult Filter()
+        {
+            return Json("ABC");
+        }
+
+    }
+}
